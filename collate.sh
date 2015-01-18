259c7021b18f47053e4db7c6ff62def3e08a1bee
matt venn <matt@mattvenn.net>
2015-01-18 18:45:45 +0000

    switch to full doc for short edge binding

--- collate.sh
+++ collate.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 #first side
-pdftk A=todo.pdf B=title.pdf C=cal_3.pdf D=biz.pdf E=cal_1.pdf F=blank.pdf cat Aeast Beast C Deast E F output side1.pdf
+#pdftk A=todo.pdf B=title.pdf C=cal_3.pdf D=biz.pdf E=cal_1.pdf F=blank.pdf cat Aeast Beast C Deast E F output side1.pdf
 
-pdfnup --no-landscape --nup 1x2 side1.pdf -o side1nup.pdf
+#pdfnup --no-landscape --nup 1x2 side1.pdf -o side1nup.pdf
 
 #second sides
-pdftk A=blank.pdf B=cal_4.pdf C=blank.pdf D=cal_2.pdf E=graph.pdf F=graph.pdf cat Aeast B Ceast D Eeast Feast output side2.pdf
+#pdftk A=blank.pdf B=cal_4.pdf C=blank.pdf D=cal_2.pdf E=graph.pdf F=graph.pdf cat Aeast B Ceast D Eeast Feast output side2.pdf
 
-pdfnup --no-landscape --nup 1x2 side2.pdf -o side2nup.pdf
+#pdfnup --no-landscape --nup 1x2 side2.pdf -o side2nup.pdf
+
+pdftk A=todo.pdf B=title.pdf E=cal_3.pdf F=biz.pdf I=cal_1.pdf J=blank.pdf C=blank.pdf D=cal_4.pdf G=blank.pdf H=cal_2.pdf K=graph.pdf L=graph.pdf cat Aeast Beast Ceast D E Feast Geast H I Jeast Keast Least output cal_a4.pdf
+
+pdfnup --no-landscape --nup 1x2 cal_a4.pdf -o cal_a5.pdf
