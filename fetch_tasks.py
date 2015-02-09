21f7dccd8dff26a4328c25a9f6c6b3d950f6b4b5
matt venn <matt@mattvenn.net>
2015-02-09 21:14:01 +0000

    fetch from 2 boards

--- fetch_tasks.py
+++ fetch_tasks.py
@@ -10,29 +10,33 @@ def auth():
     #get all the boards
     boards = client.list_boards()
     #only want the tasks board
-    board = filter(lambda x: x.name == 'tasks', boards)[0]
-    return board
+    return boards
 
 #fetch all cards in a list
-def get_tasks(board,name):
+def get_tasks(board,name,nice_name):
     ret = []
     todo = filter(lambda x: x.name == name, board.all_lists())[0]
     cards = todo.list_cards()
 
-    """
-    print("%Matt's Diary")
-    print("%")
-    print("%")
+    print('# {}'.format(nice_name))
     print("")
-    """
-    print('# Todo list')
     for card in cards:
         print( '* ' + card.name )
         ret.append(card.name)
         #ipdb.set_trace()
-
+    print("")
     return ret
 
 if __name__ == '__main__':
-    board = auth()
-    get_tasks(board,'Doing')
+    boards = auth()
+
+    print("% Todo List")
+    print("% Matt Venn")
+    print("%")
+    print("")
+
+    tasks = filter(lambda x: x.name == 'tasks', boards)[0]
+    get_tasks(tasks,'Doing','Personal')
+
+    tasks = filter(lambda x: x.name == 'cpd4t', boards)[0]
+    get_tasks(tasks,'Doing','CPD4T')
